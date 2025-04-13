3d439a082c3ffd5573abb31c9bcf03097f9f9925
ParkSinGyu <singyu13@gmail.com>
2025-04-14 00:32:01 +0900

    Chore: setting typeorm, swagger, nest/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import * as process from 'node:process';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot(
+			{
+				isGlobal: true,
+				envFilePath: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+			}
+		)
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	
+	// Setup Swagger
+	const config = new DocumentBuilder()
+		.setTitle("Gratitude-Coo API")
+		.setDescription("The API for the Gratitude-Coo application")
+		.setVersion("1.0")
+		.addTag("gratitude-coo")
+		.build();
+	
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, documentFactory);
+	
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
