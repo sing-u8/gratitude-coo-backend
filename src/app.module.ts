79093dbdf438093407c3590c643c506a557c1dd0
ParkSinGyu <singyu13@gmail.com>
2025-04-14 00:47:49 +0900

    Feat: init app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import * as process from 'node:process';
+import { z } from 'zod';
 
 @Module({
 	imports: [
@@ -10,6 +11,10 @@ import * as process from 'node:process';
 			{
 				isGlobal: true,
 				envFilePath: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+				validationSchema: z.object({
+					// Define your environment variables here
+					NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+				})
 			}
 		)
 	],
